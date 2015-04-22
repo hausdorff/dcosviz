c17d2ffbffd6f404d1763aaad3600d419d9ea552
unknown <waelra@WIN-RABADI-15-1.redmond.corp.microsoft.com>
2015-04-22 15:18:01 -0700

    Connected API to UI

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,4 +1,7 @@
-﻿MESOS_MASTER = "http://srv6.hw.ca1.mesosphere.com:5050/"
+﻿//MESOS_MASTER = "http://srv6.hw.ca1.mesosphere.com:5050/"
+//MESOS_MASTER = "http://master0-swartz3.westus.cloudapp.azure.com:5050/";
+MESOS_MASTER = "http://master0-build15dcos.southeastasia.cloudapp.azure.com:5050/";
+
 REFRESH = true;
 INTERVAL = 2000;
 
@@ -13,14 +16,15 @@ $().ready(function () {
     // 3 = cassandra
     var taskTotals = { marathon: 0, spark: 0, cassandra: 0 };
     var tasks = [];
+    var SIMULATE_BASETASK = 0;
 
     var url = MESOS_MASTER + "state.json"
 
     var STATE = [];
 
-    var fetchState = function(cb) {
-        $.getJSON(url + "?jsonp=?", function(data) {
-            var tasks = _.map(data.frameworks, function(fw) {
+    var fetchState = function (cb) {
+        $.getJSON(url + "?jsonp=?", function (data) {
+            var tasks = _.map(data.frameworks, function (fw) {
                 return {
                     "name": fw.name.split("-")[0],
                     "task_count": fw.tasks ? fw.tasks.length : 0
@@ -30,10 +34,19 @@ $().ready(function () {
             STATE.shift();
             STATE.push(tasks);
 
+            renderBlocks2();
+
             _.delay(cb, INTERVAL);
         }.bind(this));
     };
 
+    $("#set_basetask").on('click', function () {
+        var nin = prompt("How many tasks?", "1");
+        var n = new Number(nin);
+        if (n != NaN && n > 0) {
+            SIMULATE_BASETASK = n;
+        }
+    });
     $("#add_task").on('click', function () {
         if (event.shiftKey === true) {
             var nin = prompt("How many tasks?", "1");
@@ -134,6 +147,53 @@ $().ready(function () {
         }
     }
 
+    function renderBlocks2() {
+        var stateTasks = STATE[0];
+        var totalTasks = _.reduce(stateTasks, function (m, v) {
+            return m + v.task_count;
+        }, 0);
+
+        $(".total-tasks :first-child").html(totalTasks);
+        _.each(stateTasks, function (v, i) {
+            $(".slot" + i + " :first-child").html(v.task_count);
+            $(".slot" + i + " :last-child").html(v.name);
+            $(".slot" + i).show();
+        });
+
+        var canvas = $("#canvas1");
+        var context = canvas[0].getContext("2d");
+        context.canvas.height = canvas.height();
+        context.canvas.width = canvas.height();
+        context.clearRect(0, 0, canvas.width(), canvas.height());
+
+        // determine size of square
+        var area = context.canvas.width * context.canvas.height;
+        var cols = Math.ceil(Math.sqrt(totalTasks));
+        var rows = Math.ceil(totalTasks / cols);
+        var squareWidth = Math.floor(context.canvas.width / cols) - 5; // 5 is border
+        console.log("area: ", area, "cols", cols, "rows", rows, "size", squareWidth, "canvas:width", context.canvas.width, "canvas:height", context.canvas.height);
+        var x = 0;
+        var y = 0;
+        for (var i = 0; i < totalTasks; i++) {
+            idx = Math.floor((Math.random() * totalTasks) + 1);
+            if (idx <= stateTasks[0].task_count) {
+                drawSquare(context, x, y, squareWidth, getFillStyle(1));
+            } else if (idx <= (stateTasks[0].task_count + stateTasks[1].task_count)) {
+                drawSquare(context, x, y, squareWidth, getFillStyle(2));
+            } else {
+                drawSquare(context, x, y, squareWidth, getFillStyle(3));
+            }
+
+            x += squareWidth;
+            if ((x + squareWidth) > context.canvas.width) {
+                y += squareWidth + 5;
+                x = 0;
+            } else {
+                x += 5; // add space
+            }
+        }
+    }
+
     function drawSquare(context, x, y, size, fillStyle) {
         var height = size;
         var width = size;
@@ -166,9 +226,9 @@ $().ready(function () {
     }
 
     async.whilst(
-        function() { return REFRESH }.bind(this),
+        function () { return REFRESH }.bind(this),
         fetchState,
-        function() { }
+        function () { }
     );
 
     //createSample();
